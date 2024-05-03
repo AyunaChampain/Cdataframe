b344162335f58c17e921609ec0796bebde0ec4bc
orpheeteyssedre <151037799+orpheeteyssedre@users.noreply.github.com>
2024-05-03 08:33:53 +0200

    3 fonctions

--- column.c
+++ column.c
@@ -58,11 +58,32 @@ int occur(COLUMN* col, int x){
 }
 int pos_val_col(COLUMN* col, int x)
 {
-    int valeur;
-    valeur = col[x];
-    return valeur;
+    int valeur1;
+    valeur1 = col -> val[x];
+    return valeur1;
 }
 
-/*int supp_x_col(COLUMN* col, int x)
+int supp_x_col(COLUMN* col, int x)
+{
+    int y;
+    int CptSup = 0;
+    int valeur2;
+    for(y = 0; y < REALOC_SIZE ; y++){
+        valeur2 = col -> val[y];
+        if (valeur2 > x){
+            CptSup++;
+        }
+    }
+}
 int inf_x_col(COLUMN* col, int x)
-int equ_x_col(COLUMN* col, int x)*/
+{
+    int z;
+    int CptInf = 0;
+    int valeur3;
+    for(z = 0; z < REALOC_SIZE ; z++){
+        valeur3 = col -> val[z];
+        if (valeur3 < x){
+            CptInf++;
+        }
+    }
+/*int equ_x_col(COLUMN* col, int x)*/
